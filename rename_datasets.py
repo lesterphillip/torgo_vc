7d214427f79868ab9371a38670a4f7dbfd5aa578
lesterphillip <lpgvioleta@gmail.com>
2021-07-19 20:48:00 +0900

    feat: add splits to train/dev/test

--- rename_datasets.py
+++ rename_datasets.py
@@ -5,12 +5,18 @@ import os
 import glob
 import librosa
 import soundfile as sf
+from math import floor, ceil
 
 
-def open_and_save_wav(file_path, new_id):
+def open_and_save_wav(file_path, new_id, split_type):
     y, sr = librosa.load(file_path, 16000)
     general_id = file_path.split("/")[1]
-    new_path = f"output/{general_id}/{new_id}.wav"
+    new_path = f"output/{split_type}/{general_id}/{new_id}.wav"
+    make_directory = new_path.rsplit("/", 1)[0]
+
+    if not os.path.exists(make_directory):
+        os.makedirs(make_directory)
+
     sf.write(new_path, y, sr)
 
 
@@ -29,18 +35,36 @@ def process_csv_file(df_dys, df_nondys, gender):
     for index, row in tqdm(df_res.iterrows(), total=len(df_res)):
         wav_f = row["directory_x"]
         wav_fc = row["directory_y"]
+        transcript = row["transcripts"]
 
         if ".wav" not in str(wav_f) or ".wav" not in str(wav_fc):
             continue
 
+        if index <= 0.6 * floor(len(df_res)):
+            split_type = "train"
+        elif index <= 0.8 * floor(len(df_res)) and index > 0.6 * floor(len(df_res)):
+            split_type = "dev"
+        elif index <= 0.9 * floor(len(df_res)) and index > 0.8 * floor(len(df_res)):
+            split_type = "test1"
+        else:
+            split_type = "test2"
+
         new_id = str(index).zfill(4)
-        open_and_save_wav(wav_f, new_id)
-        open_and_save_wav(wav_fc, new_id)
+        # open_and_save_wav(wav_f, new_id, split_type)
+        # open_and_save_wav(wav_fc, new_id, split_type)
+
+        make_directory = "/transcripts"
+
+        
+
 
     df_res.to_csv("total_summary.csv", index=False)
 
+
 if __name__ == "__main__":
     df = pd.read_csv("transcripts.csv")
+    df = df.sample(frac=1, random_state=0).reset_index(drop=True)
+    print(df.head())
     df_dysF = df.loc[df["general_ids"] == "F"]
     df_nondysF = df.loc[df["general_ids"] == "FC"]
 
